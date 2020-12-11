3d3aeab6c10c142c51358c8d7368fec23dc519c3
Hichem Fantar <mobyfreak123@gmail.com>
2020-12-11 01:29:14 +0100

    Authentication Context & Route Guards

--- util/contextMiddleware.js
+++ util/contextMiddleware.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require("../config/env.json");
 
 module.exports = (context) => {
   if (context.req && context.req.headers.authorization) {
-    const token = context.req.headers.authorization.split("bearer ")[1];
+    const token = context.req.headers.authorization.split("Bearer ")[1];
     jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
       //   if (err) {
       //     // throw new AuthenticationError("Unauthenticated");
