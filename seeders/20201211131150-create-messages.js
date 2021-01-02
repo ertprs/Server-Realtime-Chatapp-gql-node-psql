c2d26f96cc3e336c6bb2afa18bf726b0a1384fb2
Hichem Fantar <hichemfantar2049@gmail.com>
2021-01-02 02:13:30 +0100

    Dummy data and styling

--- seeders/20201211131150-create-messages.js
+++ seeders/20201211131150-create-messages.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("messages", [
       {
-        uuid: "7648485a-6657-48d7-87d6-6a98931d3598",
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3590",
         content: "Hey Jane!",
         from: "john",
         to: "jane",
@@ -12,7 +12,7 @@ module.exports = {
         updatedAt: "2020-07-01 07:00:00",
       },
       {
-        uuid: "ae4df4f1-a428-400d-bb16-edd4237e0c47",
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3591",
         content: "Hey John, how's it going?",
         from: "jane",
         to: "john",
@@ -20,7 +20,7 @@ module.exports = {
         updatedAt: "2020-07-01 08:00:00",
       },
       {
-        uuid: "0a7c92ac-f69c-4799-8aad-9663a4afb47d",
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3592",
         content: "Not too bad, just getting to work, you?",
         from: "john",
         to: "jane",
@@ -28,7 +28,7 @@ module.exports = {
         updatedAt: "2020-07-01 09:00:00",
       },
       {
-        uuid: "240dd560-5825-4d5d-b089-12a67e8ec84c",
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3593",
         content: "I'm working from home now",
         from: "jane",
         to: "john",
@@ -36,7 +36,47 @@ module.exports = {
         updatedAt: "2020-07-01 10:00:00",
       },
       {
-        uuid: "fd4cee68-5caf-4b1b-80a9-5b9add7fd863",
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3594",
+        content: "That's cool! I'm joining the 'remote club' too",
+        from: "john",
+        to: "jane",
+        createdAt: "2020-07-01 11:00:00",
+        updatedAt: "2020-07-01 11:00:00",
+      },
+      {
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3595",
+        content: "Really? how come?",
+        from: "jane",
+        to: "john",
+        createdAt: "2020-07-01 12:00:00",
+        updatedAt: "2020-07-01 12:00:00",
+      },
+      {
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3596",
+        content: "got promoted to a consultancy role",
+        from: "john",
+        to: "jane",
+        createdAt: "2020-07-01 13:00:00",
+        updatedAt: "2020-07-01 13:00:00",
+      },
+      {
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3597",
+        content: "That's amazing!! well done!",
+        from: "jane",
+        to: "john",
+        createdAt: "2020-07-01 14:00:00",
+        updatedAt: "2020-07-01 14:00:00",
+      },
+      {
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3598",
+        content: "Thanks ;)",
+        from: "john",
+        to: "jane",
+        createdAt: "2020-07-01 15:00:00",
+        updatedAt: "2020-07-01 15:00:00",
+      },
+      {
+        uuid: "7648485a-6657-48d7-87d6-6a98931d3599",
         content: "Hey John, are you done with that task?",
         from: "boss",
         to: "john",
